bf3058b6b8fe0845791566afc02242d42c44b622
Pahril <88573655+visoredkon@users.noreply.github.com>
2026-06-28 01:18:57 +0800

    fix(drivers/egis0576): apply lemocine's fixes from marcel-wrld/fp-eh0576/pull/2

--- libfprint/drivers/egis0576.c
+++ libfprint/drivers/egis0576.c
@@ -45,7 +45,6 @@ struct _FpDeviceEgis0576
 
   gboolean      running;
   gboolean      stop;
-  gboolean      image_submitted;
 
   gboolean      has_background;
   guchar        background[EGIS0576_IMG_SIZE];
@@ -99,49 +98,189 @@ upscale_and_pad_img (const guchar *src_img, guchar *canvas)
 }
 
 static void
-normalize_img (const guchar *bg, guchar *img, gdouble *dark_portion)
+autocontrast_img (guchar *img)
 {
-  int min = 255;
+  const guchar *p = img;
+  const guchar *end = img + EGIS0576_IMG_SIZE;
+  guchar *p_out = img;
+  gdouble scale;
+  int min = EGIS0576_MAX_PIXEL_VAL;
   int max = 0;
+  int range;
 
-  // Pass 1: find diff range (bg - img)
-  for (gint i = 0; i < EGIS0576_IMG_SIZE; i++)
+  while (p < end)
     {
-      int d = (int) bg[i] - (int) img[i];
-      if (d < min)
-        min = d;
-      if (d > max)
-        max = d;
+      guchar val1 = p[0];
+      guchar val2 = p[1];
+
+      p += 2;
+
+      if (val1 < min)
+        min = val1;
+      if (val1 > max)
+        max = val1;
+      if (val2 < min)
+        min = val2;
+      if (val2 > max)
+        max = val2;
     }
 
-  max -= EGIS0576_CONTRAST;
-  min += EGIS0576_CONTRAST;
-  int range = max - min;
-  if (range <= 0)
+  range = max - min;
+  if (range < 1)
     range = 1;
 
-  // Pass 2: normalize using contrast range
-  int count_ridges = 0;
-  for (gint i = 0; i < EGIS0576_IMG_SIZE; i++)
+  scale = (gdouble) EGIS0576_MAX_PIXEL_VAL / range;
+
+  while (p_out < end)
     {
-      int d = CLAMP ((int) bg[i] - (int) img[i], min, max);
-      int normalized = 255 - ((d - min) * 255) / range;
+      p_out[0] = (guchar) ((p_out[0] - min) * scale);
+      p_out[1] = (guchar) ((p_out[1] - min) * scale);
+      p_out += 2;
+    }
+}
 
-      if (normalized < EGIS0576_RIDGE)
-        {
-          if (normalized < EGIS0576_CLAMP0)
-            normalized = 0;
-          count_ridges++;
-        }
-      else if (normalized > EGIS0576_CLAMP255)
-        {
-          normalized = 255;
-        }
+static void
+reset_session (FpDeviceEgis0576 *self)
+{
+  self->has_background = FALSE;
+  memset (self->background, 0, sizeof (self->background));
+  self->seq_type = SEQ_INIT;
+  self->seq_pkt_index = 0;
+  self->stop = FALSE;
+}
 
-      img[i] = (guchar) normalized;
+static void
+subtract_and_autocontrast (guchar *bg, const guchar *img, guchar *out, gdouble *dark_portion)
+{
+  const guchar *p_bg = bg;
+  const guchar *p_img = img;
+  const guchar *end_bg = bg + EGIS0576_IMG_SIZE;
+  const guchar *p = out;
+  const guchar *end = out + EGIS0576_IMG_SIZE;
+  guchar *p_out = out;
+  int dark = 0;
+
+  while (p_bg < end_bg)
+    {
+      int d1 = (int) p_bg[0] - (int) p_img[0];
+      int d2 = (int) p_bg[1] - (int) p_img[1];
+
+      if (d1 < 0)
+        d1 = -d1;
+      if (d2 < 0)
+        d2 = -d2;
+
+      p_out[0] = (guchar) d1;
+      p_out[1] = (guchar) d2;
+
+      p_bg += 2;
+      p_img += 2;
+      p_out += 2;
     }
 
-  *dark_portion = (gdouble) count_ridges / EGIS0576_IMG_SIZE;
+  autocontrast_img (out);
+
+  while (p < end)
+    {
+      if (p[0] < EGIS0576_DARK_THRESHOLD)
+        dark++;
+      if (p[1] < EGIS0576_DARK_THRESHOLD)
+        dark++;
+      p += 2;
+    }
+
+  *dark_portion = (gdouble) dark / EGIS0576_IMG_SIZE;
+}
+
+static gdouble
+mean_abs_diff (const guchar *bg, const guchar *img)
+{
+  const guchar *p_bg = bg;
+  const guchar *p_img = img;
+  const guchar *end_bg = bg + EGIS0576_IMG_SIZE;
+  gdouble sum = 0;
+
+  while (p_bg < end_bg)
+    {
+      int d1 = (int) p_bg[0] - (int) p_img[0];
+      int d2 = (int) p_bg[1] - (int) p_img[1];
+
+      if (d1 < 0)
+        d1 = -d1;
+      if (d2 < 0)
+        d2 = -d2;
+
+      sum += d1 + d2;
+      p_bg += 2;
+      p_img += 2;
+    }
+
+  return sum / EGIS0576_IMG_SIZE;
+}
+
+static int
+count_unique (const guchar *img, int size)
+{
+  const guchar *p = img;
+  const guchar *end = img + size;
+  const guchar *end_even = img + (size & ~1);
+  guchar seen[256];
+  int unique = 0;
+
+  memset (seen, 0, sizeof (seen));
+
+  while (p < end_even)
+    {
+      guchar val1 = p[0];
+      guchar val2 = p[1];
+
+      p += 2;
+
+      if (!seen[val1])
+        {
+          seen[val1] = 1;
+          unique++;
+        }
+      if (!seen[val2])
+        {
+          seen[val2] = 1;
+          unique++;
+        }
+    }
+
+  if (p < end)
+    {
+      guchar val = p[0];
+
+      if (!seen[val])
+        unique++;
+    }
+
+  return unique;
+}
+
+static gboolean
+capture_quality_ok (const guchar *bg, const guchar *img, const guchar *processed, gdouble dark_portion, gdouble *diff)
+{
+  *diff = mean_abs_diff (bg, img);
+
+  if (*diff < EGIS0576_MIN_ABS_DIFF)
+    return FALSE;
+  if (dark_portion < EGIS0576_DARK_PORTION_MIN || dark_portion > EGIS0576_DARK_PORTION_MAX)
+    return FALSE;
+  if (count_unique (processed, EGIS0576_IMG_SIZE) < EGIS0576_MIN_PROCESSED_UNIQ)
+    return FALSE;
+
+  return TRUE;
+}
+
+static gboolean
+bg_is_valid (const guchar *bg)
+{
+  gint variance = fpi_std_sq_dev (bg, EGIS0576_IMG_SIZE);
+  int unique = count_unique (bg, EGIS0576_IMG_SIZE);
+
+  return variance >= EGIS0576_MIN_BG_VARIANCE && unique >= EGIS0576_MIN_BG_UNIQ;
 }
 
 static void
@@ -153,55 +292,104 @@ capture_background (FpDevice *dev, FpiUsbTransfer *transfer, gdouble variance)
 
   if (variance < EGIS0576_BG_VARIANCE)
     {
+      if (!bg_is_valid (img))
+        {
+          fp_dbg ("Rejecting flat/invalid background frame (variance %.2f)", variance);
+          self->seq_type = SEQ_REPEAT;
+          fpi_ssm_next_state_delayed (transfer->ssm, EGIS0576_DELAY_SHORT_MS);
+          return;
+        }
+
       memcpy (self->background, img, EGIS0576_IMG_SIZE);
       self->has_background = TRUE;
 
       fpi_device_report_finger_status (dev, FP_FINGER_STATUS_NEEDED);
 
       self->seq_type = SEQ_REPEAT;
-      fpi_ssm_next_state_delayed (transfer->ssm, EGIS0576_DELAY_SHORT_MS);
+      fpi_ssm_next_state_delayed (transfer->ssm, EGIS0576_DELAY_CAPTURE_MS);
       return;
     }
 
-  // Variance above threshold, finger on sensor
   fpi_image_device_retry_scan (img_self, FP_DEVICE_RETRY_REMOVE_FINGER);
 
   self->seq_type = SEQ_REPEAT;
   fpi_ssm_next_state_delayed (transfer->ssm, EGIS0576_DELAY_LONG_MS);
 }
 
-static gboolean
-handle_finger_quality (FpDevice *dev, FpiUsbTransfer *transfer, gboolean finger_present, gdouble dark_portion)
-{
-  FpImageDevice *img_self = FP_IMAGE_DEVICE (dev);
-  FpDeviceEgis0576 *self = FPI_DEVICE_EGIS0576 (dev);
-
-  if (finger_present && (dark_portion < EGIS0576_DARK_PORTION_MIN || dark_portion > EGIS0576_DARK_PORTION_MAX))
-    {
-      fpi_image_device_retry_scan (img_self, FP_DEVICE_RETRY_CENTER_FINGER);
-      self->seq_type = SEQ_REPEAT;
-      fpi_ssm_next_state_delayed (transfer->ssm, EGIS0576_DELAY_LONG_MS);
-      return TRUE;
-    }
-
-  if (!finger_present)
-    {
-      self->seq_type = SEQ_REPEAT;
-      fpi_image_device_report_finger_status (img_self, FALSE);
-      fpi_ssm_next_state_delayed (transfer->ssm, EGIS0576_DELAY_SHORT_MS);
-      return TRUE;
-    }
-
-  return FALSE;
-}
-
-
 static void
-submit_finger_image (FpDevice *dev, FpiUsbTransfer *transfer)
+process_finger (FpDevice *dev, FpiUsbTransfer *transfer)
 {
   FpImageDevice *img_self = FP_IMAGE_DEVICE (dev);
   FpDeviceEgis0576 *self = FPI_DEVICE_EGIS0576 (dev);
   guchar *img = transfer->buffer;
+  guchar processed[EGIS0576_IMG_SIZE];
+  gdouble variance = fpi_std_sq_dev (img, EGIS0576_IMG_SIZE);
+
+  if (!self->has_background)
+    {
+      capture_background (dev, transfer, variance);
+      return;
+    }
+
+  FpiImageDeviceState img_state;
+  g_object_get (G_OBJECT (dev), "fpi-image-device-state", &img_state, NULL);
+
+  gboolean finger_present = FALSE;
+  gdouble dark_portion = -1;
+  gdouble mean_abs_diff_val = 0;
+
+  if (variance > EGIS0576_VARIANCE)
+    {
+      subtract_and_autocontrast (self->background, img, processed, &dark_portion);
+      finger_present = capture_quality_ok (self->background, img, processed, dark_portion, &mean_abs_diff_val);
+    }
+
+  fp_dbg ("Finger status (present, variance, dark port, abs diff, state) : %d , %.2f, %.2f, %.2f, %d",
+          finger_present, variance, dark_portion, mean_abs_diff_val, img_state);
+
+  if (img_state == FPI_IMAGE_DEVICE_STATE_AWAIT_FINGER_OFF)
+    {
+      if (variance < EGIS0576_BG_VARIANCE)
+        fpi_image_device_report_finger_status (img_self, FALSE);
+
+      self->seq_type = SEQ_POLL;
+      self->seq_pkt_index = 0;
+      fpi_ssm_next_state_delayed (transfer->ssm, EGIS0576_DELAY_POLL_MS);
+      return;
+    }
+
+  if (!finger_present)
+    {
+      if (variance > EGIS0576_VARIANCE && self->has_background)
+        {
+          fp_dbg ("Rejecting low-quality capture, recalibrating background");
+          self->has_background = FALSE;
+          fpi_image_device_retry_scan (img_self, FP_DEVICE_RETRY_REMOVE_FINGER);
+        }
+      else
+        {
+          fpi_image_device_report_finger_status (img_self, FALSE);
+        }
+
+      self->seq_type = SEQ_REPEAT;
+      fpi_ssm_next_state_delayed (transfer->ssm, EGIS0576_DELAY_POLL_MS);
+      return;
+    }
+
+  if (img_state == FPI_IMAGE_DEVICE_STATE_AWAIT_FINGER_ON)
+    {
+      fpi_image_device_report_finger_status (img_self, TRUE);
+      self->seq_type = SEQ_REPEAT;
+      fpi_ssm_next_state_delayed (transfer->ssm, EGIS0576_DELAY_MED_MS);
+      return;
+    }
+
+  if (img_state != FPI_IMAGE_DEVICE_STATE_CAPTURE)
+    {
+      self->seq_type = SEQ_REPEAT;
+      fpi_ssm_next_state_delayed (transfer->ssm, EGIS0576_DELAY_MED_MS);
+      return;
+    }
 
   FpImage *fp_img = fp_image_new (EGIS0576_CANVAS_WIDTH, EGIS0576_CANVAS_HEIGHT);
   if (!fp_img)
@@ -211,43 +399,14 @@ submit_finger_image (FpDevice *dev, FpiUsbTransfer *transfer)
       return;
     }
 
-  upscale_and_pad_img (img, fp_img->data);
+  upscale_and_pad_img (processed, fp_img->data);
 
   fpi_image_device_report_finger_status (img_self, TRUE);
   fpi_image_device_image_captured (img_self, fp_img);
+  fpi_image_device_report_finger_status (img_self, FALSE);
 
-  self->image_submitted = TRUE;
-  fpi_ssm_jump_to_state (transfer->ssm, DEV_START);
-}
-
-static void
-process_finger (FpDevice *dev, FpiUsbTransfer *transfer)
-{
-  FpDeviceEgis0576 *self = FPI_DEVICE_EGIS0576 (dev);
-  guchar *img = transfer->buffer;
-  gdouble variance = fpi_std_sq_dev (img, EGIS0576_IMG_SIZE);
-
-  if (!self->has_background)
-    {
-      capture_background (dev, transfer, variance);
-      return;
-    }
-
-  gboolean finger_present = FALSE;
-  gdouble dark_portion = -1;
-  if (variance > EGIS0576_VARIANCE)
-    {
-      normalize_img (self->background, img, &dark_portion);
-      finger_present = dark_portion > EGIS0576_DARK_PORTION;
-    }
-
-  fp_dbg ("Finger status (present, variance, dark port) : %d , %.2f, %.2f",
-          finger_present, variance, dark_portion);
-
-  if (handle_finger_quality (dev, transfer, finger_present, dark_portion))
-    return;
-
-  submit_finger_image (dev, transfer);
+  self->seq_type = SEQ_REPEAT;
+  fpi_ssm_next_state_delayed (transfer->ssm, EGIS0576_DELAY_CAPTURE_MS);
 }
 
 static void
@@ -454,12 +613,6 @@ ssm_run_state (FpiSsm *ssm, FpDevice *dev)
           return;
         }
 
-      if (self->image_submitted)
-        {
-          fpi_ssm_next_state_delayed (ssm, EGIS0576_DELAY_MED_MS);
-          return;
-        }
-
       self->seq_pkt_index = 0;
       if (self->seq_type == SEQ_IMAGE)
         self->seq_type = SEQ_REPEAT;
@@ -498,7 +651,7 @@ sm_cb (FpiSsm *ssm, FpDevice *dev, GError *error)
 
   if (self->stop)
     {
-      self->image_submitted = FALSE;
+      reset_session (self);
       fpi_image_device_deactivate_complete (img_dev, NULL);
     }
 }
@@ -531,9 +684,7 @@ dev_activate (FpImageDevice *dev)
   FpDeviceEgis0576 *self = FPI_DEVICE_EGIS0576 (dev);
   FpiSsm *ssm = fpi_ssm_new (FP_DEVICE (dev), ssm_run_state, NUM_STATES);
 
-  self->image_submitted = FALSE;
-  self->has_background = FALSE;
-  self->stop = FALSE;
+  reset_session (self);
   self->running = TRUE;
 
   fpi_ssm_start (ssm, sm_cb);
@@ -581,7 +732,7 @@ fpi_device_egis0576_class_init (FpDeviceEgis0576Class *klass)
   dev_class->id_table = id_table;
   dev_class->scan_type = FP_SCAN_TYPE_PRESS;
   dev_class->nr_enroll_stages = 40;
-  dev_class->temp_hot_seconds = 0;
+  dev_class->temp_hot_seconds = -1;
 
   img_class->img_open = dev_init;
   img_class->img_close = dev_deinit;
@@ -591,5 +742,5 @@ fpi_device_egis0576_class_init (FpDeviceEgis0576Class *klass)
   img_class->img_width = EGIS0576_CANVAS_WIDTH;
   img_class->img_height = EGIS0576_CANVAS_HEIGHT;
 
-  img_class->bz3_threshold = 9; /* security issue, can score more but not reliably */
+  img_class->bz3_threshold = 5;
 }
